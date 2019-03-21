ad356ab7c8fdbee4b0bda2376745843c03a39541
Fisher <george.kokinis@gmail.com>
2019-03-21 09:05:36 +0000

    Begin writing endpoints.

--- piapi.py
+++ piapi.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/days/", defaults={"day": None}, methods=["GET"])
+@app.route("/days/<string:day>", methods=["GET"])
+def days(day):
+    if day:
+        return day
+    else:
+        return "List of days."
+
+
+@app.route("/records/", defaults={"record": None}, methods=["GET", "POST"])
+@app.route("/records/<string:record>", methods=["GET"])
+def records(record):
+    if request.method == "POST":
+        # Get JSON and save.
+        pass
+    elif request.method == "GET":
+        if record:
+            return "Record " + record
+        else:
+            return "List of records."
